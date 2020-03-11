dddd63bee613dd1c69259bdc4bab2faa454f6471
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2020-03-11 00:29:23 +0000

    Working on tab feature to add nodes by typing their name. Need to add autocompletion support.

--- src/editor.js
+++ src/editor.js
@@ -1,7 +1,27 @@
+let currentIText = null;
+
+// ------------------------------------
+// DOM - Get the canvas
+// ------------------------------------
 let canvas = new fabric.Canvas('c');
 canvas.setWidth(window.innerWidth);
 canvas.setHeight(window.innerHeight);
 canvas.backgroundColor = '#333';
+
+
+// ------------------------------------
+// Asset loading
+// ------------------------------------
+
+// Load the nodes definitions
+fetch("example_nodes.json")
+    .then(response => response.json())
+    .then(json => {
+        addParsedNodes(json)
+    })
+    .catch(err => {
+        console.error(err)
+    });
 
 // Check the loading of the font
 // TODO: progress bar
@@ -13,7 +33,6 @@ inconsolataFont.load()
     }).catch(function(e) {
         console.error(`font loading failed ${e}`);
     });
-
 
 // ------------------------------------
 // Event Callbacks
@@ -50,43 +69,78 @@ canvas.on('mouse:wheel', function(opt) {
 //     canvas.renderAll();
 //   });
 
-document.addEventListener('keydown', (event) => {
-    switch (event.code) {
-        case 'KeyN': {
-            let inputs = ['radius', 'kernelSize'];
-            let outputs = ['image'];
+function addParsedNodes(json) {
 
-            let node = new FlowPipeNode('Blur', {inputs:inputs, outputs:outputs});
-            console.log(node);
-            canvas.add(node);
-            node.center();
-            break;
-        };
+    // Register the nodes contained in the json
+    json.forEach(node => {
+        console.log(`registering ${node.name}`);
+    });
 
-    case 'KeyF': {
-        let ao = canvas.getActiveObject();
-        if (!ao) {
-            break;
+    let form = document.getElementById('create-node');
+
+    document.addEventListener('keydown', (event) => {
+        console.log(event.code);
+
+        switch (event.code) {
+
+            // TODO: Add nodes with tab and autocompletion
+            case 'Tab': {
+                event.preventDefault();
+
+                currentIText = new fabric.IText('a', {
+                    backgroundColor: '#fff'
+                });
+                canvas.add(currentIText);
+                currentIText.center();
+                currentIText.selectAll();
+                currentIText.enterEditing();
+                console.log('entering edit');
+                break;
+            }
+
+            case 'Enter': {
+                if (currentIText){
+                    console.log('exiting edit');
+                    currentIText.exitEditing();
+                    currentIText = null;
+                }
+                break;
+            }
+
+            // Focus
+            case 'KeyF': {
+                let ao = canvas.getActiveObject();
+                if (!ao) {
+                    break;
+                }
+
+                canvas.viewportCenterObject(ao);
+                break;
+            }
+
+            // Home
+            case 'KeyH': {
+                canvas.zoomToPoint({
+                    x: canvas.width * 0.5,
+                    y: canvas.height * 0.5
+                }, 1.0);
+                break;
+            }
+
+            // Delete
+            case 'Backspace': {
+
+                // If we are not in edit mode,
+                // we want to override the default behaviour of tab
+                if (!currentIText){
+                    event.preventDefault();
+                }
+
+                canvas.getActiveObjects().forEach(element => {
+                    canvas.remove(element);
+                });
+                break;
+            };
         }
-
-        canvas.viewportCenterObject(ao);
-        break;
-    }
-
-    case 'KeyH': {
-        canvas.zoomToPoint({
-            x: canvas.width * 0.5,
-            y: canvas.height * 0.5
-        }, 1.0);
-        break;
-    }
-
-    case 'Backspace': {
-        // TODO: override the main backspace event
-        canvas.getActiveObjects().forEach(element => {
-            canvas.remove(element);
-        });
-        break;
-    };
-    }
-});
+    });
+}
